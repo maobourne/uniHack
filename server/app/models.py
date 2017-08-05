50c50d9c26fc7edadee00103742d33dac710086f
Andrew Persic <persic.andrew@gmail.com>
2017-08-05 18:52:59 +1000

    Got rid of Aaron's garbage commit

--- server/app/models.py
+++ server/app/models.py
@@ -3,15 +3,7 @@ from django.db import models
 # Create your models here.
 class Profile(models.Model):
    name = models.CharField(max_length = 50)
-<<<<<<< HEAD
-   picture = models.ImageField(upload_to = 'temp_photo')
-=======
    picture = models.ImageField(upload_to = 'static')
->>>>>>> 58c7cd32306289809a273a453626fbf39f5acb7d
-
-
-
-
 
 class someImage(models.Model):
     image = models.ImageField(upload_to= "photos")
