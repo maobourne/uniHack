9384a8557a29e3f5acbbd2455ff3343f95cdfc9e
Ryan <ryan.kingsley@outlook.com>
2017-08-05 18:36:49 +1000

    Auto stash before merge of "develop" and "origin/develop"
    Added param. in index.html, new ocr module and save  images to 'static' folder

--- server/app/models.py
+++ server/app/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Profile(models.Model):
    name = models.CharField(max_length = 50)
-   picture = models.ImageField(upload_to = 'temp_photo')
+   picture = models.ImageField(upload_to = 'static')
 
 
 
