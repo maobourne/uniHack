4d31a0eb657b0198ccae8b4df635dca248a349bb
Unknown <nxcleo@gmail.com>
2017-08-05 19:44:52 +1000

    upload is complete

--- server/app/models.py
+++ server/app/models.py
@@ -3,11 +3,11 @@ from django.db import models
 # Create your models here.
 class Profile(models.Model):
    name = models.CharField(max_length = 50)
-   picture = models.ImageField(upload_to = 'temp_photo')
+   picture = models.ImageField("img", upload_to = 'static')
 
 
 
 
 
 class someImage(models.Model):
-    image = models.ImageField(upload_to= "photos")
+    pic = models.ImageField(upload_to= "static")
--- server/app/views.py
+++ server/app/views.py
@@ -2,8 +2,9 @@ from django.shortcuts import render
 from django.core.files import File
 import os
 from django import forms
-from app.models import Profile
+from app.models import Profile, someImage
 from django.http import HttpResponse
+import uuid
 
 
 # Create your views here.
@@ -20,22 +21,30 @@ def index(request):
     saved = 0
     print("request received: "+ request.method )
     if request.method == "GET":
-        print("index...\n\n")
         return render(request, 'uploadform.html')
     elif request.method == "POST":
-        print("post received")
         imageForm = ProfileForm(request.POST, request.FILES)
-        print("got image form")
 
         if imageForm.is_valid():
-            print("form is valid")
-            profile = Profile()
-            profile.name = imageForm.cleaned_data["name"]
-            profile.picture = imageForm.cleaned_data["picture"]
-            profile.picture.save(os.path.basename(profile.name), profile.picture)
+
+            image = someImage()
+            image.pic = imageForm.cleaned_data["picture"]
+            print(image.pic.url)
+            imgType = image.pic.url[image.pic.url.rfind("."):]
+            print(imgType)
+            image.pic.save(str(uuid.uuid1()) + imgType,image.pic, False)
+            print("URL after... ", image.pic.url)
             print("saved")
             saved = 1
         else:
             imageForm = ProfileForm()
+
+
+        #upload to gdrive
+
+        #get addr of file
+
+        #call text(url)
+
 
         return HttpResponse("""<h1>file saved !</h1>""")
