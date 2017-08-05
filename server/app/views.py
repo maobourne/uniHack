81f255b51e1067f0b9887f4505e7006470d8ff5c
Andrew Persic <persic.andrew@gmail.com>
2017-08-06 05:43:10 +1000

    Revert "Auto stash before merge of "develop" and "origin/develop""
    This reverts commit 96249222c5539520900f265bc1379633b85deba3.

--- server/app/views.py
+++ server/app/views.py
@@ -12,6 +12,8 @@ from app.vision import ocr
 
 from apiclient.http import MediaFileUpload
 
+
+
 # Create your views here.
 # # Create your views here.
 # def SaveProfile(request):
@@ -19,74 +21,54 @@ from apiclient.http import MediaFileUpload
 #     return HttpResponse(text)
 
 ROOT = os.path.dirname(os.path.abspath(__file__))
-# folder_id = '0B6TQGqGzyC5rZ2NBUktISDRJRnc'
-
+folder_id = '0B6TQGqGzyC5rZ2NBUktISDRJRnc'
 
 class ProfileForm(forms.Form):
     picture = forms.ImageField()
 
 
-def getLink(imgUrl, image, folder_id):
-    imgType = imgUrl[imgUrl.rfind("."):]
-    uuuuuid = str(uuid.uuid1())
-    image.pic.save(uuuuuid + imgType, image.pic, False)
-
-    saved = 1
-    source = ROOT + "/img/" + uuuuuid + imgType
-    fname = source[source.rfind("/")+1:]
-    image_link, service = main(fname, folder_id, source)
-    return fname, image_link, service
-
 def index(request):
     saved = 0
     print("request received: " + request.method)
     if request.method == "GET":
-        if "folderid" in request.session:
-            return render(request, 'prototype.html', {"folderid": request.session["folderid"]})
-        else:
-            return render(request, 'prototype.html', {})
+        return render(request, 'prototype.html')
     elif request.method == "POST":
-        # print(request.POST)
+        print(request.POST)
         imageForm = ProfileForm(request.POST, request.FILES)
 
         imageForm.is_valid()
 
         image = someImage()
+        image.pic = imageForm.cleaned_data["picture"]
         image.url = request.POST["imgurl"]
         image.fid = request.POST["folderid"]
         image.hw = "handwritten" in request.POST
-        if request.POST["folderid"] != "":
-            request.session["folderid"] = request.POST["folderid"]
-        if "folderid" in request.session:
-            folder_id = request.session["folderid"]
-        else:
-            folder_id = "0B6TQGqGzyC5rZmFBZFdaZ2NfcnM"
 
-        # if not isinstance(image.pic, ImageField :
-        if "picture" in imageForm.cleaned_data:
-            image.pic = imageForm.cleaned_data["picture"]
-            fname, image_link, service = getLink(image.pic.url, image, folder_id)
-        elif image.url != "":
-            fname, image_link, service = getLink(img.url, image, folder_id)
-        else:
-            return render(request, 'prototype.html', {
-                "folderid": request.session["folderid"],
-                "returnText": request.session["returnText"],
-            })
-        
-        if image.hw:
-            text_output = str(text_recognition(image_link))
-        else:
-            text_output = str(ocr(image_link))
+        # if request.session
 
-        # TODO:
-        for char in text_output:
-            try:
-                char.encode("cp1521")
-            except:
-                char = "?"
-        
-        request.session["returnText"] = text_output
+        print(image.pic.url)
+        imgType = image.pic.url[image.pic.url.rfind("."):]
+        print(imgType)
+        uuuuuid = str(uuid.uuid1())
+        image.pic.save(uuuuuid + imgType, image.pic, False)
+        print("URL after... ", "\\app\\img\\" + uuuuuid + imgType)
+
+        print("saved")
+        saved = 1
+        source = ROOT + "/img/" + uuuuuid + imgType
+        fname = source[source.rfind("/")+1:]
+        image_link, service = main(fname, folder_id, source)
+
+        # upload to gdrive
+        # print(image_link)
+
+        # get addr of file
+
+        ocr_back = ocr(image_link)
+        # print(ocr_back)
+        # call text(url)
+
+        text_output = str(ocr(image_link))
 
         prefix = "https://script.google.coma/macros/student.monash.edu/s/"
 
@@ -94,12 +76,26 @@ def index(request):
 
         create_link = prefix + folder_id + suffix + text_output
 
-        return render(request, 'prototype.html', {
-            "folderid": request.session["folderid"],
-            "returnText": request.session["returnText"],
-        })
+            # print("text_out:", text_output)
 
-        # return HttpResponse(
-            # """<h1>file uploaded!</h1>
-            #    <p>Text: """ + text_output + "</p>"
-        # )
+        # textfile_path = ROOT + '/txt/' + fname[:fname.rfind(".")] + ".txt"
+
+        # with open(textfile_path, "w") as handle:
+            # handle.write(text_output)
+
+        # file_metadata = {
+            # 'name': fname[:fname.rfind(".")],
+            # 'parents': [folder_id]
+        # }
+        # media = MediaFileUpload(textfile_path,
+                                # mimetype='text/plain',
+                                # resumable=True)
+        # file = service.files().create(body=file_metadata,
+                                            # media_body=media,
+                                            # fields='id').execute()
+        # print('Textfile ID: %s' % file.get('id'))
+
+        return HttpResponse(
+            """<h1>file uploaded!</h1>
+               <p>Text: """ + text_output + "</p>"
+        )
---
+++ server/app/vision.py
@@ -0,0 +1,148 @@
+def pretty(d, indent=1):
+    out = ""
+    for key, value in d.items():
+        out += ('\n' * indent + str(key))
+        if isinstance(value, dict):
+            pretty(value, indent+1)
+        else:
+            out += ('\n' * (indent+1) + str(value))
+    return out
+
+def ocr(url):
+    ########### Optical Character Recognition (OCR) with Computer Vision API Using Python #############
+    import http.client, urllib.request, urllib.parse, urllib.error, base64, json
+
+    ###############################################
+    #### Update or verify the following values. ###
+    ###############################################
+
+    # Replace the subscription_key string value with your valid subscription key.
+    subscription_key = '44b27428b9ba42a39f53e0915a56df94'
+
+    # Replace or verify the region.
+    #
+    # You must use the same region in your REST API call as you used to obtain your subscription keys.
+    # For example, if you obtained your subscription keys from the westus region, replace
+    # "westcentralus" in the URI below with "westus".
+    #
+    # NOTE: Free trial subscription keys are generated in the westcentralus region, so if you are using
+    # a free trial subscription key, you should not need to change this region.
+    uri_base = 'westus.api.cognitive.microsoft.com'
+
+    headers = {
+        # Request headers.
+        'Content-Type': 'application/json',
+        'Ocp-Apim-Subscription-Key': subscription_key,
+    }
+
+    params = urllib.parse.urlencode({
+        # Request parameters. The language setting "unk" means automatically detect the language.
+        'language': 'unk',
+        'detectOrientation ': 'true',
+    })
+
+    # The URL of a JPEG image containing text.
+    body = "{'url':'" + url + "'}"
+
+    try:
+        # Execute the REST API call and get the response.
+        conn = http.client.HTTPSConnection('westus.api.cognitive.microsoft.com')
+        conn.request("POST", "/vision/v1.0/ocr?%s" % params, body, headers)
+        response = conn.getresponse()
+    except:
+        text_output = "Error: Connection Request Fail"
+        conn.close()
+    try:
+        data = response.read()
+        dic = json.loads(data.encode("UTF-8"))
+        text_output = ""
+        for region in dic["regions"]:
+            for line in region["lines"]:
+                for word in line["words"]:
+                    text_output += word["text"] + " "
+                    # print(word["text"])
+        # print(text_output)
+    except Exception as e:
+        print('Error:')
+        print(e)
+        # text_output = "error"
+        conn.close()
+
+    ####################################
+
+    return text_output
+
+def text_recognition(url):
+    ########### Python 3.6 #############
+    import http.client, urllib.request, urllib.parse, urllib.error, base64, requests, time, json
+
+    ###############################################
+    #### Update or verify the following values. ###
+    ###############################################
+
+    # Replace the subscription_key string value with your valid subscription key.
+    subscription_key = '44b27428b9ba42a39f53e0915a56df94'
+
+    # Replace or verify the region.
+    #
+    # You must use the same region in your REST API call as you used to obtain your subscription keys.
+    # For example, if you obtained your subscription keys from the westus region, replace
+    # "westcentralus" in the URI below with "westus".
+    #
+    # NOTE: Free trial subscription keys are generated in the westcentralus region, so if you are using
+    # a free trial subscription key, you should not need to change this region.
+    uri_base = 'https://westus.api.cognitive.microsoft.com'
+
+    requestHeaders = {
+        # Request headers.
+        # Another valid content type is "application/octet-stream".
+        'Content-Type': 'application/json',
+        'Ocp-Apim-Subscription-Key': subscription_key,
+    }
+
+    # The URL of a JPEG image containing handwritten text.
+    body = {'url' : url}
+
+    # For printed text, set "handwriting" to false.
+    params = {'handwriting' : 'true'}
+
+    try:
+        # This operation requrires two REST API calls. One to submit the image for processing,
+        # the other to retrieve the text found in the image.
+        #
+        # This executes the first REST API call and gets the response.
+        response = requests.request('POST', uri_base + '/vision/v1.0/RecognizeText', json=body, data=None, headers=requestHeaders, params=params)
+
+        # Success is indicated by a status of 202.
+        if response.status_code != 202:
+            # if the first REST API call was not successful, display JSON data and exit.
+            parsed = json.loads(response.text)
+            print ("Error:")
+            print (json.dumps(parsed, sort_keys=True, indent=2))
+            exit()
+
+        # The 'Operation-Location' in the response contains the URI to retrieve the recognized text.
+        operationLocation = response.headers['Operation-Location']
+
+        # Note: The response may not be immediately available. Handwriting recognition is an
+        # async operation that can take a variable amount of time depending on the length
+        # of the text you want to recognize. You may need to wait or retry this GET operation.
+
+        print('\nHandwritten text submitted. Waiting 10 seconds to retrieve the recognized text.\n')
+        time.sleep(10)
+
+        # Execute the second REST API call and get the response.
+        response = requests.request('GET', operationLocation, json=None, data=None, headers=requestHeaders, params=None)
+
+        # 'data' contains the JSON data. The following formats the JSON data for display.
+        parsed = json.loads(response.text)
+        print ("Response:")
+        print (json.dumps(parsed, sort_keys=True, indent=2))
+
+    except Exception as e:
+        print('Error:')
+        print(e)
+
+    ####################################
+
+    return text_output
